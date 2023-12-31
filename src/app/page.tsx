8901c997744c838956405a8a0c99d79e4da3e576
kitayoshi <midare@utakana.de>
2023-12-31 18:22:04 +0900

    chore: polish footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ function Page() {
       <CardList className={cx(styles.cardList)} />
 
       <footer className={cx(styles.footer, 'mt-auto', 'sm:mt-0')}>
-        <div className={styles.iconLinkList}>
+        {/* <div className={styles.iconLinkList}>
           <a
             className={cx(styles.iconLink)}
             href="https://github.com/kitayoshi/pacer-watch"
@@ -33,15 +33,33 @@ function Page() {
               className={cx(styles.icon, 'transition-transform-colors')}
             />
           </a>
+        </div> */}
+        <div className={styles.linkContainer}>
+          <Link
+            className={cx(styles.link, 'transition-transform-colors')}
+            href="https://pacer.watch"
+            color="foreground"
+            target="_blank"
+          >
+            https://pacer.watch
+          </Link>
+          <Link
+            className={cx(styles.link, 'transition-transform-colors')}
+            href="https://x.com/kitayoshi_son"
+            color="foreground"
+            target="_blank"
+          >
+            by Kitayoshi
+          </Link>
+          <Link
+            className={cx(styles.link, 'transition-transform-colors')}
+            href="https://github.com/kitayoshi/pacer-watch"
+            color="foreground"
+            target="_blank"
+          >
+            on GitHub
+          </Link>
         </div>
-        <Link
-          className={cx(styles.link, 'transition-transform-colors')}
-          href="https://x.com/kitayoshi_son"
-          color="foreground"
-          target="_blank"
-        >
-          by Kitayoshi
-        </Link>
       </footer>
     </main>
   )
